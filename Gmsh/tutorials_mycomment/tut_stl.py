c83ddcccd199a080eedad8d6bf6b1858d7b8cd60
NAKANISHI\sygha <syghabc@gmail.com>
2024-05-21 21:05:10 +0900

    stlについて試行(途中)

--- Gmsh/tutorials_mycomment/tut_stl.py
+++ Gmsh/tutorials_mycomment/tut_stl.py
@@ -1,5 +1,5 @@
 """
-gmshでstlのボリュームメッシュを作成
+gmshでstlのサーフェスメッシュを切りなおす。
 """
 import gmsh
 import math
@@ -14,10 +14,6 @@ curveAngle = 180 #ほぼ触らないパラメータ
 includeBoundary = True #ほぼ触らないパラメータ
 gmsh.model.mesh.classifySurfaces(angle * math.pi / 180., includeBoundary, forceParametrizablePatches, curveAngle * math.pi / 180.)
 gmsh.model.mesh.createGeometry() #classifySurfacesを事前に実行しない場合、エラーになった。
-
-s = gmsh.model.getEntities(2) #2次元の全要素を抽出。
-l = gmsh.model.geo.addSurfaceLoop([e[1] for e in s]) #全ての面を含めるsurface loopを定義。
-gmsh.model.geo.addVolume([l]) #volume定義。
 gmsh.model.geo.synchronize()
 
 # physical valueを定義。
@@ -26,14 +22,14 @@ gmsh.model.addPhysicalGroup(2, [1], name = "in1")
 gmsh.model.addPhysicalGroup(2, [2], name = "in2")
 gmsh.model.addPhysicalGroup(2, [3], name = "out")
 gmsh.model.addPhysicalGroup(2, [4], name = "side")
-gmsh.model.addPhysicalGroup(3, [1], name = "fluid")
+# gmsh.model.addPhysicalGroup(3, [1], name = "fluid")
 
 f = gmsh.model.mesh.field.add("MathEval")
-gmsh.model.mesh.field.setString(f, "F", "0.001") #3番目の引数を小さくするとメッシュ数も増える。
+gmsh.model.mesh.field.setString(f, "F", "0.005") #3番目の引数を小さくするとメッシュ数も増える。
 gmsh.model.mesh.field.setAsBackgroundMesh(f)
 
-gmsh.model.mesh.generate(3)
+gmsh.model.mesh.generate(2)
 gmsh.option.setNumber("Mesh.MshFileVersion", 2)
-gmsh.write('mixing_elbow.vtk')
+gmsh.write('test.stl')
 
 gmsh.finalize()
